feccfbe57b44df8ee101a8e64bd9fbbb8b56ee94
diyage_precision_7920 <diyage0418@163.com>
2022-07-20 19:03:51 +0800

    2022-07-20-19:03

--- Tool/BaseTools/dataset_define.py
+++ Tool/BaseTools/dataset_define.py
@@ -254,7 +254,7 @@ def get_voc_trainval_data_loader(
         batch_size: int,
         train: bool = True,
 ):
-    root_path = '{}/{}/trainval/'.format(root_path, year)
+
     normalize = transforms.Normalize(
         std=[0.5, 0.5, 0.5],
         mean=[0.5, 0.5, 0.5],
